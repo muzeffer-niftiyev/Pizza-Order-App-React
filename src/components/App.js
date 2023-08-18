5255917567031f987f22982feecb1fa51ba5ddf7
Muzeffer <muzeffer9b2004@gmail.com>
2023-08-19 00:20:23 +0400

    navbar modified

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../styles/App.css";
+import Navbar from "./Navbar";
+import Slider from "./Slider";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Slider />
+    </>
+  );
+}
---
+++ src/components/Slider.js
@@ -0,0 +1,10 @@
+import Img from "../img/slider-1.jpg";
+
+
+export default function Slider() {
+    return (
+      <div className="slider">
+        <img src={Img} alt="slider-1"/>
+      </div>
+    );
+}
