7e75ec0966d1b5dbef04b7fc7a686e7f000d11a5
Muzeffer <muzeffer9b2004@gmail.com>
2024-05-03 00:07:55 +0400

    Added pagination to products

--- src/components/menu/components/products/products.jsx
+++ src/components/menu/components/products/products.jsx
@@ -5,7 +5,12 @@ import { addToCart, ProductData } from "../../index";
 
 const Products = ({ data }) => {
   const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedOption, setSelectedOption] = useState(data.map(() => 0));
+
+  const totalPages = Math.ceil(data.length / 6);
+  const startIndex = (currentPage - 1) * 6;
+  const endIndex = Math.min(startIndex + 6, data.length);
 
   const addToCartBtnHandler = (product, index) => {
     const curOption = product.options[selectedOption[index]];
@@ -31,32 +36,56 @@ const Products = ({ data }) => {
     return product.options[selectedOption[index]];
   };
 
+  const changePageHandler = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className={styles.container}>
-      {data.map((product, index) => (
-        <div key={getSelectedProduct(product, index).id} className={styles.box}>
-          <img
-            src={
-              product.imgUrl
-                ? product.imgUrl
-                : getSelectedProduct(product, index).img
-            }
-            alt="product"
-          />
-          <ProductData
-            index={index}
-            product={product}
-            selectedOption={selectedOption}
-            changeSizeBtnHandler={changeSizeBtnHandler}
-          />
-          <button
-            className={styles.cart_btn}
-            onClick={() => addToCartBtnHandler(product, index)}
+    <div className={styles.products_cont}>
+      <div className={styles.container}>
+        {data.slice(startIndex, endIndex).map((product, index) => (
+          <div
+            key={getSelectedProduct(product, index).id}
+            className={styles.box}
           >
-            Add to cart
-          </button>
+            <img
+              src={
+                product.imgUrl
+                  ? product.imgUrl
+                  : getSelectedProduct(product, index).img
+              }
+              alt="product"
+            />
+            <ProductData
+              index={index}
+              product={product}
+              selectedOption={selectedOption}
+              changeSizeBtnHandler={changeSizeBtnHandler}
+            />
+            <button
+              className={styles.cart_btn}
+              onClick={() => addToCartBtnHandler(product, index)}
+            >
+              Add to cart
+            </button>
+          </div>
+        ))}
+      </div>
+      {totalPages > 1 && (
+        <div className={styles.product_pages}>
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+            (page) => (
+              <button
+                key={page}
+                className={currentPage === page ? styles.active_button : ""}
+                onClick={() => changePageHandler(page)}
+              >
+                {page}
+              </button>
+            )
+          )}
         </div>
-      ))}
+      )}
     </div>
   );
 };
